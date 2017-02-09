82166c06a701625318cffd7e6ad974b0b74d9317
Emi <emimlg1@gmail.com>
2017-02-09 16:59:19 +0100

    added apimanager

--- MyApplication/app/src/main/java/es/telefonica/talentum/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/es/telefonica/talentum/myapplication/MainActivity.java
@@ -3,11 +3,24 @@ package es.telefonica.talentum.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import es.telefonica.talentum.myapplication.managers.DeckApiManager;
+import es.telefonica.talentum.myapplication.model.Deck;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DeckApiManager apiManager = new DeckApiManager();
+        apiManager.setOnNewDeckListener(new DeckApiManager.DeckApiManagerNewDeckListener() {
+            @Override
+            public void onNewDeck(Deck deck) {
+
+            }
+        });
+
+        apiManager.newDeck(this);
     }
 }
